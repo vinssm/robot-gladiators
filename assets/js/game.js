8b6f85f1c20db1378ab99dd6ecec7b63f17e1236
Vinay Vallabhaneni <vinssmedia@gmail.com>
2021-11-12 07:09:47 -0500

    js initial practice

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,2 +1,29 @@
+// this creates a function named "fight"
+function fight() {
+    window.alert("The fight has begun!");
+}
 
-window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("What is your robot's name?");
+// Note the lack of quotation marks around playerName
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log("Our robot's name is " + playerName);
+
+var playerName = "Tony the Robot";
+
+//Tony the Robot is ready for battle!
+console.log("Tony the Robot" + " is ready for battle!");
+
+//Tony the Robot is ready for battle!
+console.log(playerName + " is ready for battle!");
+
+// "your robot, Tony the Robot, has won!"
+console.log("Your robot, " + playerName + ", has won!");
+
+// fight();
+
+
